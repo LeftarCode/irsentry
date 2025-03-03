ac6e868e57fcf160460a224b7f43c89bd467b949
Mateusz Lewczak <leftarcode@gmail.com>
2025-03-03 21:46:33 +0100

    Create more complex function parser

--- src/main.cpp
+++ src/main.cpp
@@ -2,39 +2,87 @@
 #include "seperated_antlr/LLVMLexer.h"
 #include "seperated_antlr/LLVMParser.h"
 #include "seperated_antlr/LLVMParserBaseVisitor.h"
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 
 using namespace antlr4;
 using namespace antlr4::tree;
 
-struct ParameterInfo {
+struct Instruction {
+  std::string text;
+};
+
+struct Parameter {
   std::string type;
   std::string name;
+};
+
+struct BasicBlock {
+  std::string label;
+  std::vector<Instruction> instructions;
 };
 
 struct FunctionInfo {
   std::string returnType;
   std::string name;
-  std::vector<ParameterInfo> parameters;
-  std::string body;
+  std::vector<Parameter> parameters;
+  std::vector<BasicBlock> basicBlocks;
 };
 
-void parseFunctionParams(LLVMParser::FunctionHeaderContext *ctx) {
+std::vector<Parameter>
+parseFunctionParameters(LLVMParser::FunctionHeaderContext *ctx) {
   if (!ctx->params()) {
-    return;
+    return {};
   }
-  auto currentParamList = ctx->params()->paramList();
 
-  int paramNum = 1;
-  while (currentParamList) {
-    auto param = currentParamList->param();
-    std::cout << paramNum << ". " << param->llvmType()->getText() << ": "
-              << param->localIdent()->getText() << std::endl;
-
-    currentParamList = currentParamList->paramList();
-    paramNum++;
+  std::vector<Parameter> params;
+  for (auto *paramChain = ctx->params()->paramList(); paramChain != nullptr;
+       paramChain = paramChain->paramList()) {
+    auto *param = paramChain->param();
+    params.emplace_back(Parameter{param->llvmType()->getText(),
+                                  param->localIdent()->getText()});
   }
+  return params;
+}
+
+BasicBlock parseBasicBlock(LLVMParser::BasicBlockContext *ctx) {
+  if (!ctx->instructions()) {
+    return {};
+  }
+
+  std::string blockLabel;
+  std::vector<Instruction> instructions;
+
+  if (auto *label = ctx->optLabelIdent()) {
+    blockLabel = label->getText();
+  }
+
+  for (auto *instChain = ctx->instructions()->instructionList();
+       instChain != nullptr; instChain = instChain->instructionList()) {
+    instructions.insert(instructions.begin(),
+                        Instruction{instChain->instruction()->getText()});
+  }
+
+  if (auto *terminator = ctx->terminator()) {
+    instructions.emplace_back(Instruction{terminator->getText()});
+  }
+
+  return BasicBlock{blockLabel, std::move(instructions)};
+}
+
+std::vector<BasicBlock>
+parseFunctionBody(LLVMParser::FunctionBodyContext *ctx) {
+  if (!ctx->basicBlockList()) {
+    return {};
+  }
+
+  std::vector<BasicBlock> blocks;
+  for (auto *blockChain = ctx->basicBlockList(); blockChain != nullptr;
+       blockChain = blockChain->basicBlockList()) {
+    blocks.insert(blocks.begin(), parseBasicBlock(blockChain->basicBlock()));
+  }
+  return blocks;
 }
 
 class FunctionExtractorVisitor : public LLVMParserBaseVisitor {
@@ -45,18 +93,16 @@ public:
   visitFunctionDef(LLVMParser::FunctionDefContext *ctx) override {
     FunctionInfo info;
 
-    auto header = ctx->functionHeader();
-    if (header) {
+    if (auto *header = ctx->functionHeader()) {
       info.returnType = header->llvmType()->getText();
       info.name = header->globalIdent()->getText();
-      std::cout << "Function " << info.name << ": " << std::endl;
-
-      parseFunctionParams(header);
+      info.parameters = parseFunctionParameters(header);
     }
 
-    if (ctx->functionBody()) {
-      info.body = ctx->functionBody()->getText();
+    if (auto *body = ctx->functionBody()) {
+      info.basicBlocks = parseFunctionBody(ctx->functionBody());
     }
+
     functions.push_back(info);
     return visitChildren(ctx);
   }
@@ -85,6 +131,23 @@ int main() {
 
   FunctionExtractorVisitor visitor;
   visitor.visit(tree);
+
+  for (auto func : visitor.functions) {
+    std::cout << "<======================================>" << std::endl;
+    std::cout << func.returnType << " " << func.name << "(";
+    for (auto param : func.parameters) {
+      std::cout << param.type << " " << param.name;
+      std::cout << ", ";
+    }
+    std::cout << ") {" << std::endl;
+    for (auto basicBlock : func.basicBlocks) {
+      std::cout << basicBlock.label << std::endl;
+      for (auto instr : basicBlock.instructions) {
+        std::cout << "\t" << instr.text << std::endl;
+      }
+    }
+    std::cout << "}" << std::endl;
+  }
 
   return 0;
 }
